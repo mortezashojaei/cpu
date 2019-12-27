af6e5e802fcb5ca15b1cf95d5d5e5dda674c986f
morteza shojaei <mshojaei179@gmail.com>
2019-12-28 02:05:52 +0330

    complete control unit with opcodes switch case

--- ControlUnit.v
+++ ControlUnit.v
@@ -11,4 +11,15 @@ module ControlUnit(
   output RegWrite
 );
 reg [8:0] outcome;
+assign  {Reg2Loc ,AluSrc ,MemtoReg ,RegWrite ,MemRead ,MemWrite ,Branch } = outcome[8:2];
+assign Aluop = outcome[1:0];
+always @( OpCode )
+casex(OpCode)
+	11'b1xx0101x000: outcome = 9'b000100010;
+        11'b11111000010: outcome = 9'b011110000;
+        11'b11111000000: outcome = 9'b110001000;
+        11'b10110100xxx: outcome = 9'b101000101;
+	11'b00000000000: outcome = 9'b000000000;
+endcase
+    
 endmodule
