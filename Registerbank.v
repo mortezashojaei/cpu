a8ae7c65cb309660e1fbf99612542a25ca7936dd
morteza shojaei <mshojaei179@gmail.com>
2019-12-28 01:59:31 +0330

    add basics of register bank

---
+++ Registerbank.v
@@ -0,0 +1,9 @@
+module registerBank(register1 ,register2 ,register3 ,datain ,clk ,regwrite ,dataout1 ,dataout2);
+input[4:0] register1;
+input[4:0] register2;
+input[4:0] register3;
+input[63:0] datain;
+input clk;
+input regwrite;
+
+endmodule
