7024a5459c60c5c35935ad858a3b36ed23f651c5
morteza shojaei <mshojaei179@gmail.com>
2019-12-28 02:00:57 +0330

    coplete register bank module

--- Registerbank.v
+++ Registerbank.v
@@ -6,4 +6,23 @@ input[63:0] datain;
 input clk;
 input regwrite;
 
-endmodule
+output[63:0] dataout1;
+output[63:0] dataout2;
+
+reg [63:0] Bank [0:31];
+
+integer  i;
+initial 
+begin
+	for( i = 0; i < 32; i = i + 1) 
+		Bank[i] = i;
+end
+always @ (posedge clk ) 
+begin
+	Bank[register3] = (regwrite == 1'b1)? datain : Bank[register3];
+end
+
+assign dataout1 = Bank[register1];
+assign dataout2 = Bank[register2];
+
+endmodule 
