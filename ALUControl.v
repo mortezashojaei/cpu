281edcdc193182db32134c2102cb26313c882ef1
morteza shojaei <mshojaei179@gmail.com>
2019-12-28 15:24:04 +0330

    add two nested switch cases in ALUCnotrol module

--- ALUControl.v
+++ ALUControl.v
@@ -4,4 +4,25 @@ module ALUControl (ALUOp, instruction, code);
     input [10:0] instruction;
     output reg [3:0] code;
 
-endmodule
+    always @ (ALUOp, instruction) begin
+
+        casex(ALUOp)
+        
+            2'b00: code = 4'b0010;
+            2'bx1: code = 4'b0111;
+            2'b1x: 
+                casex(instruction)
+
+                    11'b10001011000: code = 4'b0010;
+                    11'b11001011000: code = 4'b0110;
+                    11'b10001010000: code = 4'b0000;
+                    11'b10101010000: code = 4'b0001;
+                
+                endcase
+		default: code = 4'bz;
+        endcase
+	
+
+    end
+
+endmodule 
