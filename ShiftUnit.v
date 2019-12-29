81700f73d17e5bcf4f021662c2ba0404fb0c5afe
morteza shojaei <mshojaei179@gmail.com>
2019-12-29 10:45:28 +0330

    shif unit must shift 1 to left

--- ShiftUnit.v
+++ ShiftUnit.v
@@ -3,6 +3,6 @@ module ShiftUnit #(parameter n = 64) (input_data, output_data);
     input [n - 1 : 0] input_data;
     output[n - 1 : 0] output_data;
 
-    assign output_data = input_data << 2;
+    assign output_data = input_data << 1;
     
 endmodule
